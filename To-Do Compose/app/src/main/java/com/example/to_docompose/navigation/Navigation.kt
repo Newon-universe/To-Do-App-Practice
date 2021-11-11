840cd18a1333935cee55546eeac41915ef00d671
Newon-universe <chokovon@gmail.com>
2021-11-11 10:38:40 +0900

    Jetpack Compose / To-Do

---
+++ To-Do Compose/app/src/main/java/com/example/to_docompose/navigation/Navigation.kt
@@ -0,0 +1,42 @@
+package com.example.to_docompose.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import com.example.to_docompose.navigation.destination.listComposable
+import com.example.to_docompose.navigation.destination.splashComposable
+import com.example.to_docompose.navigation.destination.taskComposable
+import com.example.to_docompose.ui.viewmodels.SharedViewModel
+import com.example.to_docompose.util.Constants.SPLASH_SCREEN
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+
+@ExperimentalAnimationApi
+@ExperimentalMaterialApi
+@Composable
+fun SetupNavigation(
+    navController: NavHostController,
+    sharedViewModel: SharedViewModel
+) {
+    val screen = remember(navController) {
+        Screens(navController = navController)
+    }
+
+    AnimatedNavHost(
+        navController = navController,
+        startDestination = SPLASH_SCREEN
+    ) {
+        splashComposable(
+            navigateToTaskScreen = screen.splash
+        )
+        listComposable(
+            navigateToTaskScreen = screen.list,
+            sharedViewModel = sharedViewModel
+        )
+        taskComposable(
+            navigateToListScreen = screen.task,
+            sharedViewModel = sharedViewModel
+        )
+    }
+}
---
+++ To-Do Compose/app/src/main/java/com/example/to_docompose/ui/screens/task/TaskScreen.kt
@@ -0,0 +1,102 @@
+package com.example.to_docompose.ui.screens.task
+
+import android.content.Context
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import com.example.to_docompose.data.models.Priority
+import com.example.to_docompose.data.models.ToDoTask
+import com.example.to_docompose.ui.viewmodels.SharedViewModel
+import com.example.to_docompose.util.Action
+
+@Composable
+fun TaskScreen(
+    selectedTask: ToDoTask?,
+    sharedViewModel: SharedViewModel,
+    navigateToListScreen: (Action) -> Unit
+) {
+    val title: String by sharedViewModel.title
+    val description: String by sharedViewModel.description
+    val priority:Priority by sharedViewModel.priority
+
+    val context = LocalContext.current
+    
+//    BackHandler(onBackPressed = { navigateToListScreen(Action.NO_ACTION)})
+
+    BackHandler {
+        navigateToListScreen(Action.NO_ACTION)
+    }
+
+    Scaffold(
+        topBar = {
+            TaskAppBar(
+                selectedTask = selectedTask,
+                navigateToListScreen = { action ->
+                    if(action == Action.NO_ACTION) {
+                        navigateToListScreen(action)
+                    } else {
+                        if(sharedViewModel.validateFields()) {
+                            navigateToListScreen(action)
+                        } else {
+                            displayToast(context)
+                        }
+                    }
+                }
+            )
+        },
+        content = {
+            TaskContent(
+                title = title,
+                onTitleChange = {
+                    sharedViewModel.updateTitle(it)
+                },
+                description = description,
+                onDescriptionChange = {
+                    sharedViewModel.description.value = it
+                },
+                priority = priority,
+                onPrioritySelected = {
+                    sharedViewModel.priority.value = it
+                }
+            )
+        }
+    )
+}
+
+fun displayToast(context: Context) {
+    Toast.makeText(
+        context,
+        "Fields Empty",
+        Toast.LENGTH_SHORT
+    ).show()
+}
+
+//@Composable
+//fun BackHandler(
+//    backDispatcher: OnBackPressedDispatcher? =
+//        LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+//    onBackPressed: () -> Unit
+//){
+//    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
+//
+//    val backCallBack = remember {
+//        object : OnBackPressedCallback(true) {
+//            override fun handleOnBackPressed() {
+//                currentOnBackPressed()
+//            }
+//        }
+//    }
+//
+//    DisposableEffect(key1 = backDispatcher){
+//        backDispatcher?.addCallback(backCallBack)
+//
+//        onDispose {
+//            backCallBack.remove()
+//        }
+//    }
+//}
